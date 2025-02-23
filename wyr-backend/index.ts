36ed1464f42e8609cbb11d6b97afb10e3415519e
AlefAdonis <alefadonis10@gmail.com>
2025-02-23 17:46:52 -0300

    [ADD] Adding the student implementation to the app

--- wyr-backend/index.ts
+++ wyr-backend/index.ts
@@ -1,6 +1,7 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
 import professorService from "./service/ProfessorService";
+import studentService from "./service/StudentService";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use('/professor', professorService)
+app.use('/student', studentService)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
--- wyr-backend/service/ProfessorService.ts
+++ wyr-backend/service/ProfessorService.ts
@@ -8,62 +8,74 @@ const professorService = express.Router();
 const professorRepository = new ProfessorRepository();
 
 professorService.post("/", async (req, res) => {
-    try {
-        if (await professorRepository.getProfessorByEmail(req.body.data.email)) {
-            res.status(409).json({ message: "Email already exists in the system" });
-            return
-        }
-
-        const hashedPassword = await bcrypt.hash(req.body.data.password, parseInt(env.SALT || "17"));
-        const newProfessor = await professorRepository.createNewProfessor(
-            new ProfessorCreate(
-                req.body.data.firstName,
-                req.body.data.lastName,
-                req.body.data.email,
-                hashedPassword
-            )
-        );
-
-        res.status(201).json(newProfessor);
-    } catch (error) {
-        console.error(error); 
-        res.status(400).json({ message: "Couldn't create professor" });
+  try {
+    if (await professorRepository.getProfessorByEmail(req.body.data.email)) {
+      res.status(409).json({ message: "Email already exists in the system" });
+      return;
     }
+
+    const hashedPassword = await bcrypt.hash(
+      req.body.data.password,
+      parseInt(env.SALT || "17")
+    );
+    const newProfessor = await professorRepository.createNewProfessor(
+      new ProfessorCreate(
+        req.body.data.firstName,
+        req.body.data.lastName,
+        req.body.data.email,
+        hashedPassword
+      )
+    );
+
+    res.status(201).json(newProfessor);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: "Couldn't create professor" });
+  }
 });
 
-professorService.get('/all', async (_, res) => {
-    console.log("endpoint de tudo")
-    const listOfProfessors = await professorRepository.getAllProfessors();
+professorService.get("/all", async (_, res) => {
+  const listOfProfessors = await professorRepository.getAllProfessors();
 
-    if (!listOfProfessors || listOfProfessors.length === 0) {
-        res.status(204).send("No professors were found in the database");
-        return
-    }
-    res.status(200).send(listOfProfessors);
+  if (!listOfProfessors || listOfProfessors.length === 0) {
+    res
+      .status(404)
+      .send({ message: "No professors were found in the database" });
+    return;
+  }
+  res.status(200).send(listOfProfessors);
 });
 
-professorService.get(':id', async (req, res) => {
-    const id = req.params.id;
+professorService.get("/:id", async (req, res) => {
+  const id = req.params.id;
 
-    const professor = await professorRepository.getProfessorById(+id);
+  const professor = await professorRepository.getProfessorById(+id);
 
-    if (!professor) {
-        res.status(204).send(`No professor was found in the database with the specified id ${id}`);
-        return
-    }
-    res.status(200).send(professor);
+  if (!professor) {
+    res
+      .status(404)
+      .send({
+        message: `No professor was found in the database with the specified id ${id}`,
+      });
+    return;
+  }
+  res.status(200).send(professor);
 });
 
+professorService.delete("/:id", async (req, res) => {
+  const id = req.params.id;
 
-professorService.delete('/:id', async (req, res) => {
-    const id = req.params.id;
+  const professor = await professorRepository.deleteProfessorById(+id);
 
-    const professor = await professorRepository.deleteProfessorById(+id);
-
-    if (!professor){
-        res.status(204).send(`No professor were found in the database with the specified id ${id}`)
-    }
-    res.status(200).send(professor)
-})
+  if (!professor) {
+    res
+      .status(404)
+      .send({
+        message: `No professor were found in the database with the specified id ${id}`,
+      });
+    return;
+  }
+  res.status(200).send(professor);
+});
 
 export default professorService;
